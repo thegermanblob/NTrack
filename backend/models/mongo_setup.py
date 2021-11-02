feb1d8e053336bbb695d2b36e08507938464350e
Thergermanblob <thegermanblob@gmail.com>
2021-11-02 11:10:45 -0400

    Clean up

--- backend/models/mongo_setup.py
+++ backend/models/mongo_setup.py
@@ -2,7 +2,7 @@ import mongoengine
 from mongoengine.connection import connect
 
 def global_init():
-    uri = "mongodb+srv://testconsole:<password>@ntrack.chjv9.mongodb.net/Ntrack?retryWrites=true&w=majority"
+    uri = "mongodb+srv://testconsole:Miniclip1@ntrack.chjv9.mongodb.net/Ntrack?retryWrites=true&w=majority"
     #! change <password> to password
     #for cloud database use host=uri for local use db='Ntrack'
     connect(host=uri)
